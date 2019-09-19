4b0fa566521f9116b7112efe5fe282286e33c951
Albin Eriksson Waldeborn <albinwaldeborn@gmail.com>
2019-09-19 15:05:08 +0200

    Exercise3

---
+++ exercise3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne, nameTwo;
+            
+            Console.Write("Vad heter den första personen?: ");
+            nameOne = Console.ReadLine();
+
+            Console.Write("Vad heter den andra personen?: ");
+            nameTwo = Console.ReadLine();
+
+            Console.WriteLine(nameOne + " är bra på att programmera. " + nameTwo + " är riktigt  jäkla usel!");
+        }
+    }
+}
