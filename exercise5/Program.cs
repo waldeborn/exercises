8a6affc9cd0c60be7b0b3d695c92bf3fee488dcd
Albin Eriksson Waldeborn <albinwaldeborn@gmail.com>
2019-09-19 15:29:13 +0200

    Exercise5

---
+++ exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.Write("Vad heter du?: ");
+
+            name = Console.ReadLine();
+
+            Console.Write("Hur gammal är du?: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            int daysLived = age * 365;
+
+            Console.WriteLine("Hej " + name + "! Du är " + age + "år eller " + daysLived + " dagar!");
+        }
+    }
+}
