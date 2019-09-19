a1c3d56c61ecd50344d094d0671aec4a0ba7b024
lyret <viktor@lyresten.se>
2019-09-19 13:43:35 +0200

    solves exercise one

---
+++ exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Tal 1: ");
+			string inputOne = Console.ReadLine();
+			Console.Write("Tal 2: ");
+			string inputTwo = Console.ReadLine();
+
+			int numberOne = Convert.ToInt32(inputOne);
+			int numberTwo = Convert.ToInt32(inputTwo);
+			int sum = numberOne + numberTwo;
+
+            Console.WriteLine("Summan är: " + sum);
+        }
+    }
+}
