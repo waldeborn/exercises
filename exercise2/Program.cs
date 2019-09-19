55461812a7bd7f0e93b47c58eb01a1237aa4852a
Albin Eriksson Waldeborn <albinwaldeborn@gmail.com>
2019-09-19 14:48:54 +0200

    exercise2

---
+++ exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarar variabel
+            double decimalTal;
+            //Användaren får mata in sitt tal
+            Console.Write("Ange ditt decimaltal: ");
+            //Konverterar
+            decimalTal = double.Parse(Console.ReadLine());
+            //Avrundar till närmsta heltal
+            decimalTal = Math.Round(decimalTal);
+            //Utmatning på skärm
+            Console.WriteLine(decimalTal);
+        }
+    }
+}
