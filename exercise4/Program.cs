6cf1db5d607c6a027fd45c2a7772fe343cd1a93b
Albin Eriksson Waldeborn <albinwaldeborn@gmail.com>
2019-09-19 15:17:54 +0200

    Exercise4

---
+++ exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char myCharOne = 'a';
+            char myCharTwo = '2';
+
+            Console.WriteLine(myCharOne);
+            Console.WriteLine(myCharTwo);
+        }
+    }
+}
